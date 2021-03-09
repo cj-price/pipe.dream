f6d5e55fa1fda3d5d2409766080cf00c7b023249
C.J. Price <cj-price@tuta.io>
2021-03-09 18:05:01 -0500

    Bump project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pipe.dream "0.0.1"
+(defproject pipe.dream "0.0.2"
   :description "Elixir-like pipe `|>` in Clojure(Script) that supports thread-first, thread-last, and as->."
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :test-paths ["test"])
