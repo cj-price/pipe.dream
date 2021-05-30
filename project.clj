bd5c2f2b2fe670d0c72ffb1e93d3acf11a6998cb
cj-price <cj-price@tuta.io>
2021-05-30 19:55:08 -0400

    Updated project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
 (defproject pipe.dream "0.0.4"
   :description "Elixir-like pipe `|>` in Clojure(Script)."
+  :url "https://github.com/cj-price/pipe.dream"
+  :license {:name "MIT"
+            :url "https://github.com/cj-price/pipe.dream/blob/main/LICENSE"
+            :distribution :repo}
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :test-paths ["test"])
